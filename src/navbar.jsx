48a5a8264b39e7a9873038d5789a3d78a8e633cf
HadiCyb <hadihabibi2000@gmail.com>
2019-05-02 21:08:37 +0400

    bootstrap fixing

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
-import './index.css'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+//import { Navbar, Nav } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css'
 import Docs from './components/docs'
 import Description from './components/description'
@@ -9,28 +9,32 @@ import Js from './components/javascript'
 
 const routing = (
   <Router>
-    <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <Link class="navbar-brand" to='/javascript'><img src={require('./images/js.png')} style={{width:35,height:35}}/><span style={{padding:10}}>JavaScript</span></Link>
-      <ul className='navbar-nav'>
-        <li className='nav-item active'>
-          <Link className='nav-link' to="/description">Description</Link>
+  
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+       <Link className="navbar-brand" to='/javascript'><img src={require('./images/js.png')} style={{width:35,height:35}}/><span style={{padding:10}}>JavaScript</span></Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+    </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className='navbar-nav mr-auto'>
+    <li className='nav-item active'>
+    <Link className='nav-link' to="/description">Description  <span className="sr-only">(current)</span></Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/docs">Docs</Link>
+        <li className="nav-item">
+          <Link className="nav-link" to="/docs">Docs</Link>
         </li> 
         </ul>
-        <div  style={{marginLeft:770}} >
-        <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-    </div>
-     </nav>
-      <Route path="/docs" component={Docs} />
-      <Route path="/description" component={Description} />
-      <Route path="/javascript" component={Js} />
-    </div>
+      </div>
+       </nav>
+       <Route path="/docs" component={Docs} />
+       <Route path="/description" component={Description} />
+       <Route path="/javascript" component={Js} />
   </Router>
+    
 );
 export default routing;
